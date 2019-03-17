577a33b530089e89895e349b94d5b7d695df7722
58233 <58233@DESKTOP-IU9NP5A>
2019-03-17 21:18:55 +0800

    完成接入程序的开发

---
+++ src/main/java/edu/gdkm/weixin/controller/MessageReceiverController.java
@@ -0,0 +1,36 @@
+package edu.gdkm.weixin.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//控制器：负责接收用户的请求参数，调用业务逻辑层代码，返回试图/结果给客户端（浏览器）
+@RestController
+// @Controller: 基于JSP的控制器
+// @RestController: 符合RESTful风格的WEB服务的控制器
+//RESTful: 通过不同的请求方法调用不同的处理程序，返回的结果仅仅是数据，不包含视图（HTML、JSP）
+//————/zengdefeng用于后面作为路径反向代理的时候区分不同人的代码
+
+//@RequestMapping表示的含义:URL跟控制器的关系映射
+@RequestMapping("/zdf/weixin/receiver")
+public class MessageReceiverController {
+	
+	@GetMapping // 只处理Get请求
+	public String echo( //
+			String signatrue,
+			String timestamp,
+			String nonce,
+			String echostr
+			){
+		
+		//正常来讲，需要把timestamp和nonce放入一个数组，并进行排序
+		//接着把排序后的两个元素拼接成一个新的String
+		//使用SHA-1算法对新的String进行加密
+		//最后把加密的结果跟signatrue进行比较，如果相同表示验证通过，返回echostr
+		
+		
+		
+		// 原路返回echostrd的值，返回以后公众号平台就认为:服务器对接成功
+		return echostr;
+	}
+}
