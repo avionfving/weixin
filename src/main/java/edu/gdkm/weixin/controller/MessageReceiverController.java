a846386f74a7f93dccb305977cd1b372e788f129
58233 <58233@DESKTOP-IU9NP5A>
2019-03-19 00:10:03 +0800

    .................增。加。对。请。求。参。数。和。方。法。参。数。的。映。射。

--- src/main/java/edu/gdkm/weixin/controller/MessageReceiverController.java
+++ src/main/java/edu/gdkm/weixin/controller/MessageReceiverController.java
@@ -23,7 +23,6 @@ public class MessageReceiverController {
 			@RequestParam("nonce") String nonce, //
 			@RequestParam("echostr") String echostr//
 	) {
-		String str = "Hello";
 		
 		// 正常来讲，需要把timestamp和nonce放入一个数组，并进行排序
 		// 接着把排序后的两个元素拼接成一个新的String
