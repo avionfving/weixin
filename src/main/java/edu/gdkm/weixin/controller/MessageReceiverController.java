4a274b4435ceffb4020f30a88eced57f972fbc51
58233 <58233@DESKTOP-IU9NP5A>
2019-03-31 22:49:27 +0800

    消息建模、消息转换，修改pom文件配置错误

--- src/main/java/edu/gdkm/weixin/controller/MessageReceiverController.java
+++ src/main/java/edu/gdkm/weixin/controller/MessageReceiverController.java
@@ -1,5 +1,7 @@
 package edu.gdkm.weixin.controller;
 
+import javax.xml.bind.JAXB;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import edu.gdkm.weixin.domain.InMessage;
+import edu.gdkm.weixin.service.MessageTypeMapper;
 
 // 控制器 : 负责接收用户的请求参数、调用业务逻辑层代码、返回视图/结果给客户端（浏览器）
 // @Controller  基于JSP的控制器
@@ -18,7 +23,6 @@ import org.springframework.web.bind.annotation.RestController;
 // @RequestMapping表示的含义：URL跟控制器的关系映射
 @RequestMapping("/zdf/weixin/receiver")
 public class MessageReceiverController {
-	
 	
 	// 日志记录器
 	private static final Logger LOG = LoggerFactory.getLogger(MessageReceiverController.class);
@@ -54,13 +58,36 @@ public class MessageReceiverController {
 		//LOG.trace必须要求日志记录器的配置为trace级别才能输出 
 		LOG.trace("收到的消息原文：\n{}\n----------------", xml);
 		
+		
 		// 转换消息
+//		if(xml.contains("<MsgType><![CDATA[text]]></MsgType>")) {
+//			
+//		}else if (xml.contains("<MsgType><![CDATA[image]]></MsgType>")) {
+//			
+//		}else if (xml.contains("<MsgType><![CDATA[voice]]></MsgType>")) {
+//			
+//		}else if (xml.contains("<MsgType><![CDATA[video]]></MsgType>")) {
+//			
+//		}else if (xml.contains("<MsgType><![CDATA[shortvideo]]></MsgType>")) {
+//			
+//		}else if (xml.contains("<MsgType><![CDATA[location]]></MsgType>")) {
+//			
+//		}else if (xml.contains("<MsgType><![CDATA[link]]></MsgType>")) {
+//		}
+		
+		// 截取消息类型
+		String type = xml.substring(0);
+		Class<InMessage> cla = MessageTypeMapper.getClass(type);
+		
+		// 使用JAXB完成XML转换为Java对象的操作
+		InMessage inMessage = JAXB.unmarshal(xml, cla);
+		
+		LOG.debug("转换得到的消息对象 \n{}\n" , inMessage.toString());
+		
 		// 把消息丢入队列
 		// 消费队列中的消息
 		// 产生客服消息
 		return "success";
 	}
-	
-	
-	
+		
 }
