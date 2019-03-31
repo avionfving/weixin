4a06c0b3a5908e3de3772d9c68a7fb70e6afe4f2
58233 <58233@DESKTOP-IU9NP5A>
2019-03-31 22:49:27 +0800

    消息建模、消息转换

---
+++ src/main/java/edu/gdkm/weixin/domain/image/ImageInMessage.java
@@ -0,0 +1,50 @@
+package edu.gdkm.weixin.domain.image;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import edu.gdkm.weixin.domain.InMessage;
+
+@XmlAccessorType(XmlAccessType.FIELD) // JAXB从字段获取配置信息
+@XmlRootElement(name="xml") // JAXB读取XML时根元素名称
+public class ImageInMessage extends InMessage {
+	@XmlElement(name="PicUrl")
+	@JsonProperty("PicUrl")
+	private String url;
+	
+	@XmlElement(name="MediaId")
+	@JsonProperty("MediaId")
+	private String mediaId;
+	
+	public ImageInMessage() {
+		// TODO Auto-generated constructor stub
+		super.setMsgType("image");
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getMediaId() {
+		return mediaId;
+	}
+
+	public void setMediaId(String mediaId) {
+		this.mediaId = mediaId;
+	}
+
+	@Override
+	public String toString() {
+		return "ImageInMessage [url=" + url + ", mediaId=" + mediaId + ", getToUserName()=" + getToUserName()
+				+ ", getFromUserName()=" + getFromUserName() + ", getCreateTime()=" + getCreateTime()
+				+ ", getMsgType()=" + getMsgType() + ", getMsgId()=" + getMsgId() + "]";
+	}
+}
---
+++ src/main/java/edu/gdkm/weixin/service/MessageTypeMapper.java
@@ -0,0 +1,36 @@
+package edu.gdkm.weixin.service;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import edu.gdkm.weixin.domain.InMessage;
+import edu.gdkm.weixin.domain.image.ImageInMessage;
+import edu.gdkm.weixin.domain.link.LinkInMessage;
+import edu.gdkm.weixin.domain.location.LocationInMessage;
+import edu.gdkm.weixin.domain.shortvideo.ShortVideoInMessage;
+import edu.gdkm.weixin.domain.text.TextInMessage;
+import edu.gdkm.weixin.domain.video.VideoInMessage;
+import edu.gdkm.weixin.domain.voice.VoiceInMessage;
+
+public class MessageTypeMapper {
+	
+	private static Map<String, Class<? extends InMessage>> typeMap = new ConcurrentHashMap<>();
+	
+	// 通过一个Map记录了消息类型和类的关系
+	static {
+		typeMap.put("text", TextInMessage.class);
+		typeMap.put("image", ImageInMessage.class);
+		typeMap.put("voice", VoiceInMessage.class);
+		typeMap.put("video", VideoInMessage.class);
+		typeMap.put("shortvideo", ShortVideoInMessage.class);
+		typeMap.put("location", LocationInMessage.class);
+		typeMap.put("link", LinkInMessage.class);
+	}
+	
+	// 通过消息类型获取对应的类
+	@SuppressWarnings("unchecked")
+	public static <T extends InMessage>Class<T> getClass(String type){
+		return (Class<T>) typeMap.get(type);
+	}
+	
+}
